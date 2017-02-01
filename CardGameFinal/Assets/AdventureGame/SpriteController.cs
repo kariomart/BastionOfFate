dacaa21fe2520bde3692220a82a2c811ee15a5c1
Unknown <mnn264@nyu.edu>
2017-02-01 12:15:40 -0500

    fixed bugs and added stuff
    weEEEEEEE

---
+++ CardGameFinal/Assets/AdventureGame/SpriteController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteController : MonoBehaviour {
+
+	public Rigidbody2D rigid;
+
+
+	// Use this for initialization
+	void Start () {
+		rigid = GetComponent<Rigidbody2D> ();
+	}
+
+
+
+	void FixedUpdate() {
+		
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			Debug.Log("right");
+			rigid.MovePosition (transform.position + transform.forward);
+		}
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			Debug.Log ("left");
+			rigid.MovePosition (transform.position - transform.forward);
+		}
+	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ CardGameFinal/Assets/Global.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Global : MonoBehaviourSingleton <Global> {
+
+	public static Global me;
+	public PanelController battlefieldPanel;
+	public PlayerController Player;
+	public CardController Card;
+
+
+
+
+	public Global Get() {
+		if (me == null) {
+			me = (Global)FindObjectOfType(typeof(Global));
+		}
+
+		return me;
+	}
+
+
+	void Awake() {
+		Get ();
+	}
+
+
+
+	// Use this for initialization
+	void Start () {
+		SetupGame ();
+	}
+
+	void SetupGame() {
+		Player.Draw(5);
+	}
+
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
