e84e4cadc6c0c7885ca66bd41f8efea21e387290
Unknown <mnn264@nyu.edu>
2017-02-08 13:10:45 -0500

    card object and class

---
+++ CardGameFinal/Assets/CardGame/CardAsset.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class CardAsset : ScriptableObject {
+
+
+	public int health;
+	public int tokens;
+	public string name;
+	public SpriteRenderer cardBase;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+public class Card {
+
+	public int heath;
+	public int tokens;
+	public string name;
+	public SpriteRenderer cardBase;
+
+	public Card(CardAsset c)
+	{
+		health = c.heath;
+		tokens = c.tokens;
+		name = c.name;
+		cardBase = c.cardBase;
+	}
+
+}
