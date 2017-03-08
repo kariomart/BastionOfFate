a5967459d4b3733e283dc263654c86fcccb61221
Unknown <mnn264@nyu.edu>
2017-03-08 09:35:17 -0500

    organized_final_build
    yay

---
+++ CardGameFinal/Assets/Cards/Magikarp.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magikarp : Card {
+
+	public  Magikarp() {
+		name = "Magikarp";
+		health = 5;
+		damage = 0;
+	}
+
+	public override int RollDie ()
+	{
+		int roll = Random.Range (1, 7);
+
+
+		int roll1 = Random.Range (1, 7);
+		int roll2 = Random.Range (1, 7);
+		// int roll3 = Random.Range (1, 7);
+
+		if (roll1 == roll2) {
+			Debug.Log ("MAGIKARP EVOLVED INTO GYARADOS");
+			health = 10;
+			damage = 10;
+		}
+
+
+		return roll;
+
+	}
+	
+
+}
+
+
+
